f8619f022ad47dab80d871c85f24fe1df36f8492
MariAsenova <mariaasenova22@gmail.com>
2022-12-08 12:37:47 +0100

    SEP6-13 add front page dummy data, components dir and header

---
+++ components/data/front-page.js
@@ -0,0 +1,25 @@
+const frontPage = [
+    {
+        id: "01",
+        title: "Movie title",
+        img: "/images/movie-img-1",
+        category: "movie category"
+    },
+    {
+        id: "02",
+        title: "Movie title",
+        img: "/images/movie-img-1",
+        category: "movie category"
+    },
+    {
+        id: "03",
+        title: "Movie title",
+        img: "/images/movie-img-1",
+        category: "movie category"
+    },    {
+        id: "04",
+        title: "Movie title",
+        img: "/images/movie-img-1",
+        category: "movie category"
+    }
+]
