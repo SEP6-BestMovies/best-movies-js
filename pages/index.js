2a82133bfa872da3cda6fb5c11dd827c17ddd543
MariAsenova <mariaasenova22@gmail.com>
2022-12-08 23:20:32 +0100

    SEP6-13 initial set up of front page with dummy hardcoded data in app

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
 import CardComponent from '../components/UI/CardComponent';
+import SmallCardComponent from '../components/UI/SmallCardComponent';
 
 export default function Home() {
   return (
     <Fragment>
       <CardComponent />
+      <SmallCardComponent />
     </Fragment>
   );
 }
