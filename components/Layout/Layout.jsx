38997daad967312e6515978e91ec4d7511f7cad0
MariAsenova <mariaasenova22@gmail.com>
2022-12-08 23:58:11 +0100

    SEP6-13 fix import

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import { SERVER_PROPS_ID } from "next/dist/shared/lib/constants";
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 const Layout = (props) => {
     return (
