dbdbb0d1a8caa6249e5b546a0b8c0620b13a50c8
MariAsenova <mariaasenova22@gmail.com>
2022-12-08 12:48:33 +0100

    SEP6-13 make basic set up of header, layout page and footer without CSS

---
+++ components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { SERVER_PROPS_ID } from "next/dist/shared/lib/constants";
+
+const Layout = (props) => {
+    return <Fragment>
+        <Header/>
+            <div>{ props.children }</div>
+        <Footer/>
+    </Fragment>
+}
+
+export default Layout
