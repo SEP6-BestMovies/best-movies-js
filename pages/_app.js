e41c393f457262ed4a52a15e74c326d6875ef559
MariAsenova <mariaasenova22@gmail.com>
2022-12-08 12:34:58 +0100

    SEP6-13 update app and index

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'remixicon/fonts/remixicon.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
