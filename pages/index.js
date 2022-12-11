b616f9a00332a1e822275d0489590c94de751d37
MariAsenova <mariaasenova22@gmail.com>
2022-12-11 15:56:14 +0100

    add review tease section on front page only front end

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import CardComponent from '../components/UI/CardComponent';
 import SmallCardComponent from '../components/UI/SmallCardComponent';
+import ReviewsFrontPageSection from '../components/UI/ReviewsFrontPageSection';
 
 export default function Home() {
   return (
     <Fragment>
       <CardComponent />
       <SmallCardComponent />
+      <ReviewsFrontPageSection />
     </Fragment>
   );
 }
