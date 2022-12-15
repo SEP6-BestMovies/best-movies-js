fa8545673d7cd06562fd25ba058431b4eb46beda
MariAsenova <mariaasenova22@gmail.com>
2022-12-15 22:10:37 +0100

    SEP6-000 add heading for similar movies

--- components/UI/CarouselImage.jsx
+++ components/UI/CarouselImage.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import axios from 'axios';
+import { Container, Row, Col } from "reactstrap";
 import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import classes from '../../styles/carousel-image.module.css'
+import SectionSubtitle from "./SectionSubtitle";
 
 function CarouselImage(props) {
-  
+
   const [images, setImages] = useState([])
-    axios.get(`https://api.themoviedb.org/3/movie/550/similar?api_key=${process.env.apiKeyDb}`)
-      .then(response => {
-        setImages(response.data.results);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+  axios.get(`https://api.themoviedb.org/3/movie/550/similar?api_key=${process.env.apiKeyDb}`)
+    .then(response => {
+      setImages(response.data.results);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 
   const baseUrl = "https://image.tmdb.org/t/p/";
   const posterSize = "h632";
@@ -26,17 +28,24 @@ function CarouselImage(props) {
   }
 
   return (
-    <Carousel className={`${classes.carousel_top}`}>
-      {movie_paths.map((image, index) => (
-        <Carousel.Item key={index}>
-          <img
-            className={`${classes.carousel_img}`}
-            src={image}
-            alt={image.alt}
-          />
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <Container>
+      <Row>
+        <Col lg="12" md="12" className="mb-5">
+          <SectionSubtitle subtitle="Similar movies" className="align-center"/>
+        </Col>
+      </Row>
+      <Carousel className={`${classes.carousel_top}`}>
+        {movie_paths.map((image, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className={`${classes.carousel_img}`}
+              src={image}
+              alt={image.alt}
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </Container>
   );
 };
 
