58836310af2b5c49649d1144297fa1d53721e042
MariAsenova <mariaasenova22@gmail.com>
2022-12-15 14:04:13 +0100

    SEP6-17 add similar images in carousel

--- components/UI/CarouselImage.jsx
+++ components/UI/CarouselImage.jsx
@@ -4,8 +4,8 @@ import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function CarouselImage(props) {
-  const [images, setImages] = useState([]);
-
+  
+  const [images, setImages] = useState([])
     axios.get(`https://api.themoviedb.org/3/movie/550/similar?api_key=${process.env.apiKeyDb}`)
       .then(response => {
         setImages(response.data.results);
@@ -16,19 +16,25 @@ function CarouselImage(props) {
 
   const baseUrl = "https://image.tmdb.org/t/p/";
   const posterSize = "h632";
-  const imgPath = baseUrl + posterSize + images.poster_path;
+
+  const movie_paths = [];
+  for (let index = 0; index < images.length; index++) {
+    const element = images[index];
+    const imgPath = baseUrl + posterSize + element.poster_path;
+    movie_paths.push(imgPath)
+  }
 
   return (
     <Carousel>
-      {images.map((image, index) => (
+      {movie_paths.map((image, index) => (
         <Carousel.Item key={index}>
           <img
             className="d-block w-100"
-            src={imgPath}
+            src={image}
             alt={image.alt}
           />
           <Carousel.Caption>
-            <h3>{image.title}</h3>
+            <h3>{image}</h3>
             <p>{image.description}</p>
           </Carousel.Caption>
         </Carousel.Item>
